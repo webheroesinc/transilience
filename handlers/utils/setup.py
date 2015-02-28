2547df8eb55ff3e5ffc10d4d278fe8d19f10615e
Matthew Brisebois <matthew@webheroes.ca>
2015-02-28 00:49:48 +0000

    Made mongrel2-transceiver into a pypi package

---
+++ handlers/utils/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name                        = "mongrel2-transceiver",
+    packages                    = [
+        "mongrel2_transceiver",
+    ],
+    package_dir                 = {
+        "mongrel2_transceiver":		".",
+        "mongrel2_transceiver/testing":	"./testing",
+    },
+    version                     = "0.1.0",
+    description                 = "Non-blocking Mongrel2 handler transceiver",
+    author                      = "Matthew Brisebois",
+    author_email                = "matthew@webheroes.ca",
+    url                         = "https://github.com/webheroesinc/transilience",
+    keywords                    = ["mongrel2", "zmq", "transceiver", "websocket", "http"],
+    classifiers                 = [],
+)
