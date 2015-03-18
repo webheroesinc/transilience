57b39ba71f4afa6218c2646785377a9c7852ff5c
Matthew Brisebois <matthew@webheroes.ca>
2015-03-18 19:51:27 +0000

    Finished first version of db_mapper o successfully joining 2 tables with a has middle-man and returning proper structure

--- handlers/test_mapper.py
+++ handlers/test_mapper.py
@@ -2,29 +2,11 @@
 from utils		import db_mapper
 
 import simplejson	as json
-import MySQLdb		as mysqldb
-import MySQLdb.cursors	as mysql_cursors
-
-mysql_ip	= "172.17.1.13"
-db		= mysqldb.connect( host		= mysql_ip,
-                                   user		= "root",
-                                   passwd	= "tesla",
-                                   db		= "transilience",
-                                   cursorclass	= mysql_cursors.DictCursor )
-curs		= db.cursor()
 
 root		= db_mapper.load('../mapper.json')
+print root
 
-# print root
-
-data		= json.loads(str(root))
-print json.dumps(data, indent=4)
-
-query, args		= root('/movie/100')
-print query
-
-curs.execute(query, args)
-print json.dumps(curs.fetchall(), indent=4)
-
+roots		= root('/movie/100')
+print json.dumps(roots, indent=4)
 # print root('/movie/100/categories/20000/name')
 
--- handlers/utils/db_mapper.py
+++ handlers/utils/db_mapper.py
@@ -2,97 +2,196 @@
 import sys, os
 import json
 
+import MySQLdb		as mysqldb
+import MySQLdb.cursors	as mysql_cursors
 
-class Node(object):
+mysql_ip	= "172.17.1.13"
+db		= mysqldb.connect( host		= mysql_ip,
+                                   user		= "root",
+                                   passwd	= "tesla",
+                                   db		= "transilience",
+                                   cursorclass	= mysql_cursors.DictCursor )
+curs		= db.cursor()
 
-    def __init__(self, data, name='root', parent=None):
+
+class View(object):
+
+    def __init__(self, data, name='root', parents=None):
         self.name		= name
-        self.parent		= parent
-        self.table		= None
-        self.key		= None
-        self.has		= None
-        self.default		= None
-        self.children		= {}
+        self._parents		= parents or []
+        self._directives	= {}
+        self._columns		= {}
+        self._params		= {}
 
         for k,v in data.items():
-            print "Processing key: {0} value type: {1}".format(k, type(v))
             if k.startswith('.'):
-                print "Found directive"
-                setattr(self, k.strip('.'), v)
-            elif k.startswith('$'):
-                print "Found default"
-                self.default		= Node(v,k,parent=self)
+                print "Directive:	%s" % (k,)
+                if k == ".table":
+                    self.table,_,self.alias	= v.partition(':')
+                elif k == ".segments":
+                    segs		= []
+                    for var in v:
+                        var,_,t		= var.partition(':')
+                        segs.append((var,t))
+                    v			= segs
+                d			= k.strip('.')
+                self._directives[d]	= v
+                
+            elif type(v) is dict and v.get('.table') is not None:
+                print "Sub table:	%s" % (k,)
+                self._columns[k]	= View(v,k,parents=self._parents+[self])
+            else:
+                print "Column value:	%s => %s" % (k,v)
+                self._columns[k]	= v
+
+
+    def columns(self, columns=None):
+        table		= self.alias or self.table
+        clist		= ["`%s`.`%s`" % (table, self._directives.get('key'))]
+        jlist		= []
+        columns		= columns or self._columns
+        for k,v in columns.items():
+            if type(v) is View:
+                c,j	= v.columns()	
+                clist	= clist + c
+                jlist	= jlist + j
             elif type(v) is dict:
-                print "Child node"
-                self.children[k]	= Node(v,k,parent=self)
+                clist	= clist + self.columns(v)[0]
+            elif type(v) in [str,unicode]:
+                clist.append("`%s`.`%s` as `%s`" % (table, k, v))
             else:
-                print "Child value"
-                self.children[k]	= v
+                clist.append("`%s`.`%s`" % (table, k))
 
-    def __getitem__(self, key):
-        return self.children.get(key, self.default)
+        joins		= self._directives.get('has', {})
+        for k,v in joins.items():
+            t,_,a	= k.partition(':')
+            jlist.append("JOIN `%s` %s ON %s = %s" % ((t,a or '')+tuple(v)))
 
-    def columns(self):
-        columns		= []
-        table		= self.parent.table
-        for k,v in self.children.items():
-            if type(v) is Node:
-                print v.columns()
-            elif  type(v) is str:
-                columns.append("`%s`.`%s` as `%s`" % (table, k, v))
-            else:
-                columns.append("`%s`.`%s`" % (table, k))
-        return columns
+        if self._directives.get('join') is not None:
+            jlist.append("JOIN `%s` %s ON %s = %s" % ((self.table,self.alias or '')+tuple(self._directives.get('join'))))
 
-    def query(self, node):
-        columns		= node.columns()
+        return clist, jlist
+
+    def query(self):
+        columns, joins	= self.columns()
         columnstr	= ", ".join(columns)
+        joinsstr	= "\n          ".join(joins)
         
-        table		= node.parent.table
-        key		= node.parent.key
+        table		= self.table
+        key		= self._directives.get('key')
+        where		= self._directives.get('where').keys()[0]
         
         query		= """
-        SELECT %s from `%s`
+        SELECT %s from `%s` %s
+          %s 
          WHERE `%s`.`%s` = %%s
-        """ % (columnstr, table, table, key)
+        """ % (columnstr, table, self.alias or '', joinsstr, self.alias or table, where)
+        print query
         return query
 
     def build(self, path):
-        print "Getting path: {0}".format(path)
         segments	= path.strip('/').split('/')
         print "Found segments: {0}".format(segments)
-        node		= self
-        args		= []
-        for seg in segments:
-            node	= node[seg]
-            if node.name.startswith('$'):
-                args.append(seg)
 
-        query		= self.query(node)
-        return query, args
+        # Start off with the list of root views
+        # If segments: use only that view
+        # For all root views: build querys
+        fmt		= self._directives.get('format', 'list')
+        if len(segments):
+            seg		= segments.pop(0)
+            view	= root_view	= self.get(seg)
+            while segments:
+                if view is not None:
+                    for var,fmt in view.segments():
+                        view.param(var, (len(segments) or None) and segments.pop(0))
+                else:
+                    raise Exception("Endpoint does not exist: %s" % (path,))
+                if segments:
+                    view	= view.get(segments.pop(0))
+            
+            curs.execute( root_view.query(), tuple(root_view._params.values()) )
+            result		= root_view.group_data(curs.fetchall(), format=fmt)
+        else:
+            for root,root_view in self._columns.items():
+                curs.execute( root_view.query(), tuple(root_view._params.values()) )
+                roots[root]	= root_view.group_data(curs.fetchall(), format=fmt)
+            
+        return result
 
+    def group_data(self, data, format=None):
+        format			= format if format is not None else self._directives.get('format')
+        key			= self._directives.get('key')
+        if type(self._columns.get(key)) in [str,unicode]:
+            key			= self._columns.get(key)
+        key_id			= None
+        groups			= []
+        for row in data:
+            if key_id != row[key]:
+                if key_id is not None:
+                    groups.append(group)
+                group		= []
+                key_id		= row[key]
+            group.append(row)
+        groups.append(group)
+
+        rows			= []
+        for group in groups:
+            datum	= self.attach(group[0], self._columns, group)
+            rows.append( datum )
+
+        if format == "dict":
+            result	= (len(rows) or None) and rows.pop(0)
+        else:
+            result	= rows
+
+        return result
+
+    def attach(self, data, columns, rows=None):
+        struct		= columns.copy()
+        for k,v in struct.items():
+            if v == True:
+                struct[k]	= data[k]
+            elif type(v) is dict:
+                struct[k]	= self.attach(data,v)
+            elif type(v) in [str,unicode]:
+                struct[v]	= data[v]
+                del struct[k]
+            elif type(v) is View:
+                struct[k]	= v.group_data(rows)
+            else:
+                struct[k]	= None
+        return struct
+
+    def param(self, k, v):
+        self._params[k]	= v
+
+    def get(self, k, *args):
+        return self._columns.get(k,*args)
+
+    def segments(self):
+        return self._directives.get('segments', [])
+                
     def __call__(self, path):
-        query, args	= self.build(path)
-        return query, args
+        return self.build(path)
 
-    def deobject(self):
-        childs		= {}
-        for k,v in self.children.items():
-            childs[k]			= v.deobject() if type(v) is Node else v
-        if self.default is not None:
-            childs[self.default.name]	= self.default.deobject()
-        return childs
-
+    def format(self):
+        fobj		= {}
+        for k,v in self._columns.items():
+            if type(v) is View:
+                fobj['View('+k+')']	= v.format()
+            else:
+                fobj[k]			= v
+        return fobj
+                
     def __str__(self):
-        o	= self.deobject()
-        return json.dumps(o)
+        return json.dumps(self.format(), indent=4)
 
-
+    
 def load(fpath):
     with open(fpath, 'r') as f:
         data	= json.loads(f.read())
         
-    return Node(data)
+    return View(data)
 
     # self.node('/movie/100')
     # 
@@ -101,4 +200,14 @@ def load(fpath):
     #        on movies.movie_id			= movie_has_category.movie_id
     #   join categories
     #        on categories.category_id		= movie_has_category.category_id
+    #  where movie_id = $movie
+    # 
+    # select title, description, year, rating, length, categories.name, categories.description from movies
+    #   join categories
+    #        on categories.movie_id		= movies.movie_id
+    #  where movie_id = $movie
+    # 
+    # select title, description, year, rating, length, categories.name, categories.description from movies
+    #   join categories
+    #        on categories.category_id		= movies.category_id
     #  where movie_id = $movie
